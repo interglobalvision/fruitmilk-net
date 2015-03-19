840c4d1746b115c92760b0f65113ff6b9fed9fb3
mikey <michaelrayvon@gmail.com>
2015-03-19 12:16:37 -0600

    updates post types

--- lib/post-types.php
+++ lib/post-types.php
@@ -101,49 +101,6 @@ function register_cpt_installation() {
     register_post_type( 'installation', $args );
 }
 
-add_action( 'init', 'register_cpt_installation' );
-
-function register_cpt_installation() {
-
-    $labels = array( 
-        'name' => _x( 'Installations', 'installation' ),
-        'singular_name' => _x( 'Installation', 'installation' ),
-        'add_new' => _x( 'Add New', 'installation' ),
-        'add_new_item' => _x( 'Add New Installation', 'installation' ),
-        'edit_item' => _x( 'Edit Installation', 'installation' ),
-        'new_item' => _x( 'New Installation', 'installation' ),
-        'view_item' => _x( 'View Installation', 'installation' ),
-        'search_items' => _x( 'Search Installations', 'installation' ),
-        'not_found' => _x( 'No Installations found', 'installation' ),
-        'not_found_in_trash' => _x( 'No Installations found in Trash', 'installation' ),
-        'parent_item_colon' => _x( 'Parent Installation:', 'installation' ),
-        'menu_name' => _x( 'Installations', 'installation' ),
-    );
-
-    $args = array( 
-        'labels' => $labels,
-        'hierarchical' => false,
-        
-        'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'menu_position' => 5,
-        
-        'show_in_nav_menus' => true,
-        'publicly_queryable' => true,
-        'exclude_from_search' => false,
-        'has_archive' => true,
-        'query_var' => true,
-        'can_export' => true,
-        'rewrite' => true,
-        'capability_type' => 'post'
-    );
-
-    register_post_type( 'installation', $args );
-}
-
 add_action( 'init', 'register_cpt_press' );
 
 function register_cpt_press() {
