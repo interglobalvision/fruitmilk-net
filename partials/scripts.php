7b6cdb03700be94a0bcea1b373461b303550ee59
cas <casska@gmail.com>
2015-03-30 14:58:15 -0600

    add - moves matter/nav code to its own file

--- partials/scripts.php
+++ partials/scripts.php
@@ -2,5 +2,6 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
   <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/matter.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/nav.js"></script>
   <?php wp_footer(); ?>
 </section>
