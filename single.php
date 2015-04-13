524ed5cae5d6b15c57c3f1600fe66adcde1da28b
mikey <michaelrayvon@gmail.com>
2015-04-13 16:58:01 -0500

    fixes masonry class

--- single.php
+++ single.php
@@ -26,7 +26,7 @@ get_header();
 <?php
 if( have_posts() ) {
 ?>
-    <div class="container masonry">
+    <div class="container js-masonry">
       <div class="grid-sizer"></div>
       <div class="gutter-sizer"></div>
 <?php
