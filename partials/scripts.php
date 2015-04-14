832ee66966da2c264c62dfd71bd53c3d4b5d4c61
mikey <michaelrayvon@gmail.com>
2015-04-14 15:26:17 -0500

    adds library and main js

--- partials/scripts.php
+++ partials/scripts.php
@@ -2,6 +2,8 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
   <script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/matter.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/library.js"></script>
   <script src="<?php bloginfo('stylesheet_directory'); ?>/js/nav.js"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/js/main.js"></script>
   <?php wp_footer(); ?>
 </section>
