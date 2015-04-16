9311efb19ac713c4c555b92251f3dfd68faee488
mikey <michaelrayvon@gmail.com>
2015-04-15 20:12:11 -0500

    fixes error centered

--- 404.php
+++ 404.php
@@ -4,12 +4,12 @@ get_header();
 
 <!-- main content -->
 
-<section id="main-content" class="u-align-center">
+<section id="main-content">
 
     <div class="container">
       <div class="row">
         <div class="col col2">
-          <h1>Not found :(</h1>
+          <h1 class="u-align-center">Not found :(</h1>
         </div>
       </div>
     </div>
