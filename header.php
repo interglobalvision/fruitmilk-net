16701fd0fd63070fd132d8aa5c7d46261d98bdb3
mikey <michaelrayvon@gmail.com>
2015-04-16 11:46:18 -0500

    sets title as blog name

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title><?php wp_title('|',true,'right'); bloginfo('name'); ?></title>
+  <title><?php bloginfo('name'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="dns-prefetch" href="//www.instagram.com">
